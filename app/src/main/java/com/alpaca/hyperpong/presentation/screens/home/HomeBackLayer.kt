68a6cd4921e19625e3acaf952a45adc2581533cf
Lucas <lucasyavar@gmail.com>
2023-09-04 08:59:16 -0300

    Refatorando função de arquivo público para função privada.

--- app/src/main/java/com/alpaca/hyperpong/presentation/screens/home/HomeBackLayer.kt
+++ app/src/main/java/com/alpaca/hyperpong/presentation/screens/home/HomeBackLayer.kt
@@ -1,14 +1,21 @@
 package com.alpaca.hyperpong.presentation.screens.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -20,10 +27,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.alpaca.hyperpong.R
 import com.alpaca.hyperpong.domain.model.Evento
+import com.alpaca.hyperpong.presentation.common.DropdownFilterButton
 import com.alpaca.hyperpong.presentation.common.FilterChipRow
-import com.alpaca.hyperpong.presentation.common.HomeFilterButtons
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -63,7 +73,7 @@ fun HomeBackLayer(
     if (showDatePicker) {
         DatePickerDialog(
             onDismissRequest = { showDatePicker = false },
-            confirmButton = { showDatePicker = false }) {
+            confirmButton = { }) {
             DatePicker(state = datePickerState)
         }
     }
@@ -105,4 +115,54 @@ private fun HomeBackLayerContent(
         )
         FilterChipRow(items = listOf("Futuros", "Concluídos"))
     }
+}
+
+@Composable
+private fun HomeFilterButtons(
+    modalidades: List<String>,
+    categorias: List<String>? = null,
+    onDateButtonClicked: () -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(space = 12.dp)
+    ) {
+        DropdownFilterButton(
+            modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
+            title = "Escolher Modalidades",
+            leadingIcon = painterResource(id = R.drawable.ic_joystick),
+            listItens = modalidades
+        )
+        categorias?.let {
+            DropdownFilterButton(
+                modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
+                title = "Selecionar Categorias",
+                leadingIcon = painterResource(id = R.drawable.ic_settings_slow_motion),
+                listItens = it
+            )
+        }
+        FilledTonalButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(shape = MaterialTheme.shapes.small)
+                .background(color = MaterialTheme.colorScheme.surface),
+            onClick = { onDateButtonClicked() },
+            shape = MaterialTheme.shapes.small,
+            contentPadding = PaddingValues(
+                horizontal = 12.dp,
+                vertical = 12.dp
+            )
+        ) {
+            Icon(
+                tint = MaterialTheme.colorScheme.onSurface,
+                painter = painterResource(id = R.drawable.ic_calendar),
+                contentDescription = "Ícone Joystick"
+            )
+            Spacer(modifier = Modifier.size(width = 12.dp, height = 0.dp))
+            Text(
+                modifier = Modifier.weight(1f),
+                text = "Selecionar Datas"
+            )
+        }
+    }
+    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
 }
